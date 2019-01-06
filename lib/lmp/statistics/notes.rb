2afa9dde71da52aeb7ef3c65d8a02a27b20542ba
Ryan Krafnick <kraflab@gmail.com>
2019-01-06 22:43:17 +0100

    Fix stroller check

--- lib/lmp/statistics/notes.rb
+++ lib/lmp/statistics/notes.rb
@@ -9,7 +9,7 @@ module LMP
 
       def analyze_frame(frame)
         @turbo = true if frame.run.abs > 50 || frame.strafe.abs > 50
-        @stroller = false if frame.strafe != 0
+        @stroller = false if frame.strafe != 0 || frame.run.abs > 25
         @sr50_on_turns = true if frame.sr50? && frame.turn != 0
       end
 
