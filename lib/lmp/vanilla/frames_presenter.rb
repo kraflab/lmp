896acc957aa89fc48cc89f0198558bcdaf007a9f
Ryan Krafnick <kraflab@gmail.com>
2019-01-26 22:40:24 +0100

    Improve frame dump format

--- lib/lmp/vanilla/frames_presenter.rb
+++ lib/lmp/vanilla/frames_presenter.rb
@@ -4,7 +4,7 @@ module LMP
       extend self
 
       def call(demo)
-        puts 'Frame Run Strafe Turn Pause Save Fire Use Weapon'
+        puts 'Frame Run Strafe Turn Extra'
         demo.frames.each_with_index do |frame, index|
           puts dump(frame, index)
         end
@@ -13,17 +13,36 @@ module LMP
       private
 
       def dump(frame, index)
+        start = [
+          formatted_index(index),
+          formatted_speeds(frame)
+        ].join(' ')
+
+        "#{start} | #{formatted_extra(frame)}"
+      end
+
+      def formatted_index(index)
+        index = index.to_s.rjust(5)
+      end
+
+      def formatted_speeds(frame)
         [
-          index,
           frame.run,
           frame.strafe,
-          frame.turn,
-          frame.pause,
-          frame.save,
-          frame.fire,
-          frame.use,
-          frame.weapon
-        ].map(&:to_s).join(' ')
+          frame.turn
+        ].map { |x| x.to_s.rjust(4) }.join(' ')
+      end
+
+      def formatted_extra(frame)
+        extra = [
+          frame.pause ? 'pause' : nil,
+          frame.save ? 'save' : nil,
+          frame.fire ? 'fire' : nil,
+          frame.use ? 'use' : nil,
+          frame.weapon ? "weapon(#{frame.weapon})" : nil
+        ].reject(&:nil?).join(' ')
+
+        extra.length > 0 ? extra : '-'
       end
     end
   end
