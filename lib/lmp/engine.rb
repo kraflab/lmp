d12fa33e5bf62babc5e632c1e55b3ee1a8bd43c9
kraflab <kraflab@gmail.com>
2020-05-24 00:16:26 +0200

    Handle unversioned lmps

--- lib/lmp/engine.rb
+++ lib/lmp/engine.rb
@@ -9,7 +9,11 @@ module LMP
 
     def detect(filename)
       file = File.open(filename, 'rb')
-      file.getbyte # version
+      version = file.getbyte
+
+      # If it predates versioning, this is the skill field (0 - 4)
+      return :unknown if version < 5
+
       file.getbyte == 0x1D ? SIGNATURE[file.getbyte.chr] : :doom
     end
   end
