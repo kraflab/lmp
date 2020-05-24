d44ad991d997be15db503fc8521d67676448e868
kraflab <kraflab@gmail.com>
2020-05-24 15:30:36 +0200

    Make use of heretic input fields

--- lib/lmp/heretic/frame.rb
+++ lib/lmp/heretic/frame.rb
@@ -3,6 +3,8 @@ require_relative 'frame_presenter'
 module LMP
   module Heretic
     class Frame < Vanilla::Frame
+      attr_reader :look, :fly, :artifact
+
       def initialize(file, longtics, prev_frame, index)
         super
         return if end_of_frames?
@@ -10,10 +12,14 @@ module LMP
         parse_artifacts(file)
       end
 
+      def to_s(options = {})
+        FramePresenter.call(self, options)
+      end
+
       private
 
       def parse_advanced_movement(file)
-        file.getbyte # ignoring for now
+        advanced_movement = file.getbyte
 
         # looking: lower four bits
         # 0 nothing
@@ -26,11 +32,12 @@ module LMP
         # 1-7 flying up
         # 8 dropping to ground
         # 9-15 flying down (subtract 16)
+
+        @look = advanced_movement_value(advanced_movement & 15)
+        @fly = advanced_movement_value(advanced_movement >> 4)
       end
 
       def parse_artifacts(file)
-        file.getbyte # ignoring for now
-
         # 0 nothing
         # 1 ring of invulnerability
         # 2 shadowsphere
@@ -43,6 +50,16 @@ module LMP
         # 9 wings of wrath
         # 10 chaos device
         # 255 unsuccessful use attempt
+
+        @artifact = file.getbyte
+        @artifact = false if artifact == 0
+      end
+
+      def advanced_movement_value(value)
+        return false if value == 0 # no input
+        return 0 if value == 8 # (land / neutral look)
+        return value - 16 if value > 8 # signed 4 bit
+        value
       end
     end
   end
